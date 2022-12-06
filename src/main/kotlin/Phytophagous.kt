20298e7066d016f1da97ecdaabd4f10a6033e75c
KVAMulticom <kva-09@mail.ru>
2022-12-06 18:49:20 +0300

    new1

--- src/main/kotlin/Phytophagous.kt
+++ src/main/kotlin/Phytophagous.kt
@@ -1,2 +1,14 @@
-class Phytophagous {
+/*
+Травоядность — питание животных растительной пищей.
+Фитофаги являются так называемыми первичными потребителями.
+Из пищеварительных ферментов у них преобладают амилазы.
+*/
+
+class Phytophagous(id: Int, name: String, maxSpeed: Int, eatMethod: String = "Травоядное") : Animals(id, name, maxSpeed){
+
+    fun nameUpper(): String {
+        return name.uppercase()
+    }
+
 }
+//
--- src/main/kotlin/Zoophagous.kt
+++ src/main/kotlin/Zoophagous.kt
@@ -1,2 +1,14 @@
-class Zoophagous {
+/*
+Плотоядные или зоофаги - животные, питающиеся преимущественно мясом.
+Отсутствует амилаза — фермент, содержащийся в слюне, способствующий
+расщеплению пищи растительного происхождения в процессе жевания.
+*/
+
+class Zoophagous(id: Int, name: String, maxSpeed: Int) : Animals(id, name, maxSpeed) {
+
+    fun nameUpper(): String {
+        return name.uppercase()
+    }
+
 }
+//
